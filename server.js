442f8021daf87b3cebe9c3172dc09a3601134131
Felicia Walcott <59152656+fwalcott@users.noreply.github.com>
2020-10-17 18:30:04 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
 
 // Application Setup
 const PORT = process.env.PORT;
@@ -28,39 +29,62 @@ function aboutUsHandler(request, response) {
 }
 
 // API Routes
-app.get('/location', handleLocation); {
-  let city = request.query.city;
-  let data = require('./data/location.json')[0];
-  let location = new Location(data, city);
-  response.send(location);
-}; 
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather); 
 
-app.get('/restaurants', handleRestaurants);
-
-app.use('*', notFoundHandler);
-
-// HELPER FUNCTIONS
-
-function handleLocation(request, response) {
+  
+//Location Handler
+function handleLocation(req, res) {
   try {
     const geoData = require('./server/data/location.json');
-    const city = request.query.city;
+    const city = req.query.city;
     const locationData = new Location(city, geoData);
-    response.send(locationData);
+    res.send(locationData);
   }
   catch (error) {
     console.log('ERROR', error);
     response.status(500).send('So sorry, something went wrong.');
   }
 }
+//Weather Handler 
+function handleWeather (req, res){
+  try {
+    const forecastArr = []; 
+    let forcast = require('./data/weather.json'); 
+    forecast.data.forEach(value => {
+      forecastArr.push(new Weather(value));
+    });  
+    response.send(forecastArr);
+  }
+  catch(error){
+      error500();
+    }
+  }
 
- function Location(city, geoData) {
+
+// app.get('/restaurants', handleRestaurants);
+
+app.use('*', notFoundHandler);
+
+
+//Constructor Functions  
+
+//Location Function
+function Location(city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
-}  
+}   
 
+//Weather Function
+function Weather(obj){
+  this.forecast= obj.weather.description;
+  this.time = new Data(obj.valid_date).toDateString();                     
+} 
+  
+
+/* 
 function handleRestaurants(request, response) {
   try {
     const data = require('./server/data/restaurants.json');
@@ -74,19 +98,13 @@ function handleRestaurants(request, response) {
     console.log('ERROR', error);
     response.status(500).send('So sorry, something went wrong.');
   }
-}
+} */
 
-function Location(obj, query){
-  this.lat = obj.lat;
-  this.lon = obj.lon;
-  this.search_query = query;
-  this.location = obj.display_name;
-}
 
-function Restaurant(entry) {
+/* function Restaurant(entry) {
   this.restaurant = entry.restaurant.name;
   this.cuisines = entry.restaurant.cuisines;
-  this.locality = entry.restaurant.location.locality;
+  this.locality = entry.restaurant.location.locality; */
 } 
 
 
@@ -94,7 +112,8 @@ function notFoundHandler(request, response) {
   response.status(404).send('huh?');
 }
 
-
+function error500(){
+  return response.status(500).send('Sorry, something went wrong, ...')};
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
